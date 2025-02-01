b32f84c49bbb1863916604c4b11f12f8fe7a26e2
s_erfani <saeederfani75@gmail.com>
2025-02-01 15:07:47 +0330

    update .gitignore

--- src/app/services/background.service.ts
+++ src/app/services/background.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {environment} from '../../enviroments/enviroments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackgroundService {
-  private accessKey = 'RGvXvjypwre1gzqert_hZJVU0Ky_7CROGdT8WS88gzw';
-  private apiUrl = `https://api.unsplash.com/photos/random?client_id=${this.accessKey}&query=calm&orientation=landscape&count=1`;
+  private apiUrl = `https://api.unsplash.com/photos/random?client_id=${environment.unsplashApiKey}&query=calm&orientation=landscape&count=1`;
 
   constructor(private http: HttpClient) {}
 
